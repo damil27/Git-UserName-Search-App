c319308f0e9f122fb1c033dce27be29d807647fd
Damilare Fadodun <dhamilare27@gmail.com>
2021-11-21 11:19:25 +0100

    debugin setEffect errors.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     console.log(val);
     const getApi = async () => {
       const resp = await axios.get(`https://api.github.com/users/${val}`);
-      setUsers([resp.data, ...users]);
+      setUsers((users) => [resp.data, ...users]);
     };
     if (val != null) {
       getApi();
@@ -33,53 +33,3 @@ const App = () => {
 };
 
 export default App;
-
-// class App extends React.Component {
-//   state = {
-//     data: [
-//       {
-//         login: "mojombo",
-//         id: 1,
-//         node_id: "MDQ6VXNlcjE=",
-//         avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-//         gravatar_id: "",
-//         url: "https://api.github.com/users/mojombo",
-//         company: "https://github.com/mojombo",
-//       },
-//       {
-//         login: "defunkt",
-//         id: 2,
-//         node_id: "MDQ6VXNlcjE=",
-//         avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-//         gravatar_id: "",
-//         url: "https://api.github.com/users/mojombo",
-//         company: "Facebook",
-//       },
-//     ],
-//   };
-
-//   handleNewUser = (profileData) => {
-//     this.setState((prevState) => ({ data: [profileData, ...prevState.data] }));
-//     console.log(profileData);
-//   };
-
-//   render() {
-//     return (
-//       <div className="container">
-//         <Form onSubmit={this.handleNewUser} />
-//         {this.state.data.map((profile) => (
-//           <Card
-//             key={profile.id}
-//             onName={profile.login}
-//             onCompany={
-//               profile.company === null ? "Not available" : profile.company
-//             }
-//             onImage={profile.avatar_url}
-//           />
-//         ))}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
